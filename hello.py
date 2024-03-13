43f13a658519a444693497e56c298a1d090dc6e0
kevin <kevin.martin.albert@gmail.com>
2024-03-13 23:16:34 +0100

    bad guys

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
-supernanas_list = ["belle", "bulle", "rebelle"]
+supernanas_list = ["belle", "bulle", "rebelle","poubelle"]
 for supernana in supernanas_list:
     print(supernana)
+badguys_list = ["Mojojojo", "le gang des véreux", "Lui"]
+for badguy in badguys_list:
+    print(badguy)
